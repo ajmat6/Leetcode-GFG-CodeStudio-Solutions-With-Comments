6772e7abd4e3d354453e7e47b4fa19bfd3c298aa
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-06-03 10:21:02 +0530

    3rd June 2023

---
+++ Trie.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+
+class TrieNode
+{
+    public:
+        char data;
+        TrieNode* children[26];
+        bool isTerminal;
+
+        // Initialisation:
+        TrieNode(char ch)
+        {
+            this -> data = ch;
+            for(int i=0; i<26; i++)
+            {
+                children[i] = NULL;
+            }
+
+            isTerminal = false; // Initializing the isTerminal data memeber
+        }
+};
+
+class Trie
+{
+    public:
+        TrieNode* root;
+
+        Trie()
+        {
+            root = new TrieNode('\0');  // passing null character to the root
+        }
+
+        void insertCharacter(TrieNode* root, string word)
+        {
+            // base case:
+            if(word.length() == 0)
+            {
+                root -> isTerminal = true; // if insertion process is complete then making the isTerminal memeber true.
+                return;
+            }
+
+            // Assuming the word will be in CAPITAL LETTERS:
+            int index = word[0] - 'A'; // Finding the index to check for present
+            TrieNode* child;
+
+            // present then aaghe badho
+            if(root -> children[index] != NULL)
+            {
+                child = root -> children[index];
+            } 
+
+            // Not present then insert
+            else
+            {
+                child = new TrieNode(word[0]);
+                root -> children[index] = child;
+            }
+
+            // Now Recursion will handle rest of the cases:
+            insertCharacter(child, word.substr(1)); // word ke 1st index se substring
+        }
+
+        void insertWord(string word)
+        {
+            insertCharacter(root, word);
+        }
+
+        bool SearchCharacter(TrieNode* root, string word)
+        {
+            // Base case:
+            if(word.length() == 0)
+            {
+                return root -> isTerminal; // to check for the terminal node
+            }
+
+            int index = word[0] - 'A';
+            TrieNode* child;
+
+            // Present condition
+            if(root -> children[index] != NULL)
+            {
+                child = root -> children[index];
+                return SearchCharacter(child, word.substr(1));
+            }
+
+            // if not present return false:
+            return false;
+        }
+
+        bool SeachWord(string word)
+        {
+            return SearchCharacter(root, word);
+        }
+};
+
+int main()
+{
+    Trie* t = new Trie();
+
+    t -> insertWord("AJMAT");
+    t -> insertWord("ABS");
+    t -> insertWord("CAT");
+    t -> insertWord("DU");
+    t -> insertWord("TIME");
+
+    cout << "Checking for the presence of the word in the trie: "<< t -> SeachWord("CAR") << endl;
+    t -> insertWord("CAR");
+    cout << "Checking for the presence of the word in the trie: "<< t -> SeachWord("CAR") << endl;
+
+
+    return 0;
+}
